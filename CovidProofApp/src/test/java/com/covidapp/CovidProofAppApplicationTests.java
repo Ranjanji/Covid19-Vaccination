d1c52f96bbfb39ff61718731f8ff4c8878fd5fa6
bob <bob@>
2022-12-14 22:31:26 +0530

    intital

---
+++ CovidProofApp/src/test/java/com/covidapp/CovidProofAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidProofAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
