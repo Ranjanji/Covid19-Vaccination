ca42cf978572f99b29904c492b40997f1a35f268
bob <bob@>
2022-12-14 14:29:39 +0530

    Initial Commit

---
+++ CovidProofApp/src/main/java/org/CovidProof/CovidProofAppApplication.java
@@ -0,0 +1,13 @@
+package org.CovidProof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidProofAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidProofAppApplication.class, args);
+	}
+
+}
