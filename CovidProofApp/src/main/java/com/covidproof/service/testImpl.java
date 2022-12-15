6b69cc90096ea2065cacb2f6d170ef6981508597
sumitpanchal08 <sumitpanchal9034@gmail.com>
2022-12-15 09:36:11 +0530

    updated

--- CovidProofApp/src/main/java/com/covidproof/service/testImpl.java
+++ CovidProofApp/src/main/java/com/covidproof/service/testImpl.java
@@ -2,13 +2,13 @@ package com.covidproof.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.covidproof.dao.dao;
+import com.covidproof.dao.AdminDAO;
 import com.covidproof.model.Entity.Admin;
 
 @Service
 public class testImpl implements testService{
 	@Autowired
-	private dao dao;
+	private AdminDAO dao;
 
 	@Override
 	public String saveAd(Admin a) {
