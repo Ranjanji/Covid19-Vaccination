c5497cd88db4a24aee287ff7c64efec653ae388d
Subham Ranjan <subhamranjanhalo@gmail.com>
2022-12-16 08:47:26 +0530

    admin dto added login and password

--- CovidProofApp/src/main/java/com/covidproof/model/DTO/AdminPasswordDTO.java
+++ CovidProofApp/src/main/java/com/covidproof/model/DTO/AdminPasswordDTO.java
@@ -17,16 +17,16 @@ import lombok.ToString;
 @ToString
 @Getter
 @Setter
-public class AdminDTO {
-	@NotNull
-	@NotBlank
-	@NotEmpty
+public class AdminPasswordDTO {
+	@NotNull(message = "Mobile Number cannot be null.")
+	@NotBlank(message = "Mobile Number cannot be blank.")
+	@NotEmpty(message = "Mobile Number cannot be empty.")
 	@Size(min=10,max=10, message="Mobile number should be of 10digit")
 	private String mobile;
-	@NotNull
+	@NotNull(message = "Password cannot be null.")
 	@Pattern(regexp = "^[a-zA-Z0-9]{8}",message="Length length must be 8 character")
 	private String old_pass;
-	@NotNull
+	@NotNull(message = "Password cannot be null.")
 	@Pattern(regexp = "^[a-zA-Z0-9]{8}",message="Length length must be 8 character")
 	private String new_pass;
 }
